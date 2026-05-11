22474fb68a83cc31e49d915a9aace46e095e2e89
Hanumantha Reddy <hanumanthareddy@gmail.com>
2026-05-11 11:32:45 +0530

    fix: preserve reduced motion support
    Signed-off-by: Hanumantha Reddy <hanumanthareddy@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -44,7 +44,7 @@ import "@fontsource/dm-sans/700.css";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <MotionConfig reducedMotion={config.reducedMotion}>
+      <MotionConfig reducedMotion="user">
         <App />
       </MotionConfig>
     </ErrorBoundary>
